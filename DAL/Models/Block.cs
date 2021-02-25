e0760ad5c0c5221324bef5c7568984e45822d485
jkseo0225@gmail.com <jkseo0225@gmail.com>
2021-02-26 02:18:12 +0900

    2021-02-26 02:20

--- DAL/Models/Block.cs
+++ DAL/Models/Block.cs
@@ -7,13 +7,19 @@ namespace DAL.Models
     public class Block
     {
         public int Id { get; set; }
+        [Display(Name = "블럭명")]
         public string BlockName { get; set; }
 
+        [Display(Name = "일반관(소형물량)")]
         public int TargetSmallGen { get; set; }
+        [Display(Name = "선급관(소형물량)")]
         public int TargetSmallSun { get; set; }
+        [Display(Name = "일반관(대형물량)")]
         public int TargetBigGen { get; set; }
+        [Display(Name = "선급관(대형물량)")]
         public int TargetBigSun { get; set; }
 
+        [Display(Name = "소계(물량)")]
         public int TargetTotalSum
         {
             get
@@ -21,63 +27,76 @@ namespace DAL.Models
                 return (this.TargetSmallGen + this.TargetSmallSun + this.TargetBigGen + this.TargetBigSun);
             }
         }
+        [Display(Name = "총 잔량")]
         public int RestedTotalSum
         {
             get
             {
-                return this.TargetTotalSum - (this.ProductGenSum + this.ProductSunSum);
+                return (this.TargetTotalSum - (this.ProductGenSum + this.ProductSunSum));
 
             }
         }
 
+        [Display(Name = "공정율(%)")]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:N1}")]
         public decimal Percent
         {
             get
             {                
-                return (this.TargetTotalSum - this.RestedTotalSum) / this.TargetTotalSum;
+                return (decimal)(((this.TargetTotalSum - this.RestedTotalSum) / this.TargetTotalSum) * 100);
             }
         }
 
+        [Display(Name = "소형(일반관생산)")]
         public int ProductGenSmall { get; set; }
+        [Display(Name = "대형(일반관생산)")]
         public int ProductGenBig { get; set; }
+        [Display(Name = "소계(일반관생산)")]
         public int ProductGenSum
         {
             get
             {
-                return this.ProductGenSmall + this.ProductGenBig;
+                return (this.ProductGenSmall + this.ProductGenBig);
             }
         }
+        [Display(Name = "잔량(일반관생산)")]
         public int ProductGenRested
         {
             get
             {
-                return (this.TargetSmallGen + this.TargetBigGen) - this.ProductGenSum;
+                return ((this.TargetSmallGen + this.TargetBigGen) - this.ProductGenSum);
             }
         }
 
+        [Display(Name = "소형(선급관생산)")]
         public int ProductSunSmall { get; set; }
+        [Display(Name = "대형(선급관생산)")]
         public int ProductSunBig { get; set; }
+        [Display(Name = "소계(선급관생산)")]
         public int ProductSunSum
         {
             get
             {
-                return this.ProductSunSmall + this.ProductSunBig;
+                return (this.ProductSunSmall + this.ProductSunBig);
             }
         }
+        [Display(Name = "잔량(선급관생산)")]
         public int ProductSunRested
         {
             get
             {
-                return (this.TargetSmallSun + this.TargetBigSun) - this.ProductSunSum;
+                return ((this.TargetSmallSun + this.TargetBigSun) - this.ProductSunSum);
             }
         }
 
+        [Display(Name = "납기예정일")]
         [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}")]
         public DateTime TargetDate { get; set; }
 
+        [Display(Name = "프로젝트Id")]
         [Required]
         public int ProjectId { get; set; }
+        [Display(Name = "프로젝트")]
         [ForeignKey("ProjectId")]
         public Project Project { get; set; }
 
--- DAL/Models/Project.cs
+++ DAL/Models/Project.cs
@@ -11,13 +11,21 @@ namespace DAL.Models
     {
         public int Id { get; set; }
 
+        [Display(Name = "PROJECT(공사명)")]
         [Required, StringLength(30)]
         public string ProjectName { get; set; }
+        [Display(Name = "CLIENT(발주처)")]
         public string ProjectClient { get; set; }
+        [Display(Name = "CLASS(선급)")]
         public string ProjectClass { get; set; }
 
+        [Display(Name = "제작 착수일")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
         public DateTime StartDate { get; set; }
+        [Display(Name = "제작 마감일")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
         public DateTime EndDate { get; set; }
+        [Display(Name = "이벤트")]
         public string ProjectEvent { get; set; }
 
         [InverseProperty("Project")]
